55022c03249092e8830cc3f30a4802e52f56083a
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2025-01-04 17:43:09 +0700

    feat: setup layout and install radix theme (#1)
    Co-authored-by: dungtm-mc <dungtm@missingcorner.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Theme } from '@radix-ui/themes'
+import { Layout } from './components/layout'
+import { MainApp } from './components/app'
+
 function App() {
-  return <div className="text-red-500">hello hellohello hello hellooooo</div>
+  return (
+    <Theme grayColor="mauve">
+      <Layout>
+        <MainApp />
+      </Layout>
+    </Theme>
+  )
 }
 
 export default App
